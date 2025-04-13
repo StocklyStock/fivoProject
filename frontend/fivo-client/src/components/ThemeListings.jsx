1038e754b726d1f5b883ee5e6d4efa32e5ea00c5
jaeyun <wodbs0723@ut.ac.kr>
2025-04-13 22:44:18 +0900

    feat: 테마 클릭 시 관련 뉴스 및 종목 fetch 연동

--- frontend/fivo-client/src/components/ThemeListings.jsx
+++ frontend/fivo-client/src/components/ThemeListings.jsx
@@ -1,20 +1,19 @@
-const ThemeListings = () => {
+const ThemeListings = ({ stock_list  = [] }) => {
+    
+    console.log("🧪 ThemeListings에 전달된 종목:", stock_list); // 확인용
+    
     return (
         <div>
-            <div className="headline-wrap"><h2><span>추천 테마</span></h2><button>상세보기 </button></div>
+            <div className="headline-wrap"><h2><span>관련 종목</span></h2><button>상세보기 </button></div>
             <div className="list-wrap">
                 <ul>
-                    <li>테마 리스트1</li>
-                    <li>테마 리스트1</li>
-                    <li>테마 리스트1</li>
-                    <li>테마 리스트1</li>
-                    <li>테마 리스트1</li>
-                    <li>테마 리스트1</li>
-                    <li>테마 리스트1</li>
-                    <li>테마 리스트1</li>
-                    <li>테마 리스트1</li>
-                    <li>테마 리스트1</li>
-                    <li>테마 리스트1</li>
+                    {stock_list.map((stock, index) => (
+                        <li key={index}>
+                            <a href={stock.link} target="_blank" rel="noopener noreferrer">
+                                {stock.name}
+                            </a>
+                        </li>
+                    ))}
                 </ul>
             </div>
         </div>
--- frontend/fivo-client/src/pages/HomePage.jsx
+++ frontend/fivo-client/src/pages/HomePage.jsx
@@ -10,6 +10,7 @@ const HomePage = () => {
   const [selectedThemeCode, setSelectedThemeCode] = useState(null);
   const [selectedThemeName, setSelectedThemeName] = useState("");
   const [newsData, setNewsData] = useState([]);
+  const [stockList, setStockList] = useState([]);
     return (
       <main>
         {/* <h1 className="text-2xl p-10">🏠 홈 화면 - 누구나 접근 가능</h1> */}
@@ -22,10 +23,15 @@ const HomePage = () => {
         onNewsFetched={(news) => {
           setNewsData(news)
         }}
+        onStocksFetched={(stock_list) =>{
+          setStockList(stock_list)
+        }}
       />
         <ChatBotModal />
         <div className='brand-bg-color main-listings'>
-          <ThemeListings />
+          <ThemeListings
+            stock_list={stockList}
+          />
           <NewsListings
             themeCode={selectedThemeCode}
             themeName={selectedThemeName}
